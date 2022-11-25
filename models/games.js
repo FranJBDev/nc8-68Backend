e5efd27c2fa753e902a072c987d4e4cd2aaf4752
gandhyman <gandhycoder@gmail.com>
2022-11-25 09:55:07 -0700

    Add: Description in gamebyid backend route and bcrypt y json web token

--- models/games.js
+++ models/games.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
   name: String,
   fav: Number,
   sales: Number,
   stars: Array, // promedio,
-  reviews: Array,
-})
+  reviews: Array, // devolver en ruta gamebyid
+});
 
-module.exports = mongoose.model('users', userSchema)
+module.exports = mongoose.model('users', userSchema);
